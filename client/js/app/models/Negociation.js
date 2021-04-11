4a0cfd113175d30fa4b2cd5c594e64322d9df674
Guilherme Cazaroto <gui.cazaroto@gmail.com>
2021-04-10 23:28:55 -0300

    feat: index db understanding

--- client/js/app/models/Negociation.js
+++ client/js/app/models/Negociation.js
@@ -3,7 +3,6 @@ class Negociation {
     this._date = new Date(date.getTime())
     this._quantity = quantity
     this._value = value
-    this._volume =  quantity * value
     Object.freeze(this) // impede modificações diretas nos atributos da classe
   }
 
@@ -21,9 +20,5 @@ class Negociation {
 
   get value () {
     return this._value
-  }
-
-  get volume () {
-    return this._volume
   }
 }
