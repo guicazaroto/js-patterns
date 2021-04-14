ef3578249cbc546c387e2a5c851c1cde1c9d6716
Guilherme Cazaroto <gui.cazaroto@gmail.com>
2021-04-14 19:27:29 -0300

    feat: module pattern

--- client/js/app/services/ConnectionFactory.js
+++ client/js/app/services/ConnectionFactory.js
@@ -1,37 +1,44 @@
-const dbname = 'aluraframe'
-const version = 1
-const stores = ['negociations']
+const ConnectionFactory = (() => { 
 
-class ConnectionFactory {
-  constructor () {
-    throw new Error('Is not possible instatiated this class!')
+  const dbname = 'aluraframe'
+  const version = 1
+  const stores = ['negociations']
+  let connection = null
+  return class ConnectionFactory {
+    constructor () {
+      throw new Error('Is not possible instatiated this class!')
+    }
+
+    static getConnection () {
+      return new Promise((resolve, reject) => {
+        const db = indexedDB.open(dbname, version)
+    
+        db.onupgradeneeded = e => {
+          ConnectionFactory._createTable(e.target.result)
+        }
+    
+        db.onsuccess = e => {
+          if(!connection) {
+            connection = e.target.result
+          }
+          resolve(connection)
+        }
+
+        db.onerror = e => {
+          reject(e.target.result)
+        }
+      })
+    }
+
+    static _createTable (db) {
+      stores.forEach(store => {
+        if(db.objectStoreNames.contains(store)) {
+          db.deleteObjectStore(store)
+        }
+
+        db.createObjectStore(store, { autoincrement: true })
+      })
+    }
   }
 
-  static getConnection () {
-    return new Promise((resolve, reject) => {
-      const db = indexedDB.open(dbname, version)
-  
-      db.onupgradeneeded = e => {
-        ConnectionFactory._createTable(e.target.result)
-      }
-  
-      db.onsuccess = e => {
-        resolve(e.target.result)
-      }
-
-      db.onerror = e => {
-        reject(e.target.result)
-      }
-    })
-  }
-
-  static _createTable (db) {
-    stores.forEach(store => {
-      if(db.objectStoreNames.contains(store)) {
-        db.deleteObjectStore(store)
-      }
-
-      db.createObjectStore(store, { autoincrement: true })
-    })
-  }
-}
+})()
